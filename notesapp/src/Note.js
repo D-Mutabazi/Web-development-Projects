2807d38bee3e04addffe7ddc0c57b1bb528296a8
Dgibrilly Manirakiza Mutabazi <23765518@sun.ac.za>
2024-01-03 11:13:42 +0200

    Date, last modified, title sections of components updated

--- notesapp/src/Note.js
+++ notesapp/src/Note.js
@@ -1,14 +1,17 @@
-function Note (){
+function Note ( {note} ){
     return (
         <div className="app-sidebar-note">
             <div className="sidebar-note-title">
-                <strong>TITLE</strong>
+                <strong>{note.title}</strong>
                 <button>DELETE </button>
             </div>
 
-            <p>Note Preview</p>
+            <p>{note.body && note.body.substr(0,50)+ "..."}</p>
 
-            <small className="note-meta">Last modified [date]</small>
+            <small className="note-meta">Last Modified: {new Date(note.lastModified).toLocaleDateString("en-GB", {
+                hour:"2-digit",
+                minute:"2-digit"
+            })}</small>
         </div>
     )
 }
--- notesapp/src/NoteList.js
+++ notesapp/src/NoteList.js
@@ -3,9 +3,9 @@ function NoteList ( {notes} ) {
     return (
         <div className="app-sidebar-notes">
             {/* for each note in the array output all of its content */}
-            {notes.map((notes)=>(
-                /* individual note */
-                <Note/>
+            {notes.map((note)=>(
+                /* individual note displayed with respectibve components */
+                <Note note={note}/>
 
             ))}
 
