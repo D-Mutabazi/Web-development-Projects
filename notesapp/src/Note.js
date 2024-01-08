fc465c07f4dd3ce2d2709a3ba74b2cf2d2bd15c8
Dgibrilly Manirakiza Mutabazi <23765518@sun.ac.za>
2024-01-09 01:42:31 +0200

    Notes grid layout set

--- notesapp/src/Note.js
+++ notesapp/src/Note.js
@@ -3,19 +3,25 @@ function Note ( {note, onDeleteNote} ){
         <div className="app-sidebar-note">
             <div className="sidebar-note-title">
                 <strong>{note.title}</strong>
-                {/* reference needed to specify which note is to be deleted (i.e. note ID), to ensure the delete does not run automatically
-                    the delete function is binded to a arrow function
-                */}
-                <button onClick={() => onDeleteNote(note.id)}>DELETE</button>  
+               
 
             </div>
 
             <p>{note.body && note.body.substr(0,50)+ "..."}</p>
 
-            <small className="note-meta">Last Modified: {new Date(note.lastModified).toLocaleDateString("en-GB", {
-                hour:"2-digit",
-                minute:"2-digit"
-            })}</small>
+            <div className="note-footer">
+                <small className="note-meta">Last Modified: {new Date(note.lastModified).toLocaleDateString("en-GB", {
+                    hour:"2-digit",
+                    minute:"2-digit"
+                })}</small>
+
+                  {/* reference needed to specify which note is to be deleted (i.e. note ID), to ensure the delete does not run automatically
+                    the delete function is binded to a arrow function
+                */}
+                <button onClick={() => onDeleteNote(note.id)}>DELETE</button> 
+
+            </div>
+           
         </div>
     )
 }
